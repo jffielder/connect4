e6be6239aa352d9da06f825dd321be5d79413497
jffielder <jfielder0@gmail.com>
2017-07-19 03:12:21 -0400

    finished

--- lib/Connect4.rb
+++ lib/Connect4.rb
@@ -1,26 +1,104 @@
 #ruby/rspec/connect_four/lib/Connect4.rb
 
 class Connect4
+  attr_accessor :board
 
-
-  def create_board
-    Board.new
+  def initialize
+    @board = Board.new
   end
 
+  def add_piece(col)
+    @board.add_to(col)
+  end
+
+  def check_win
+    return @board.won?
+  end
+
+  def draw_board
+    @board.draw
+  end
 
 end
 
 
 class Board
-  attr_accessor :spaces, :color
+  attr_accessor :spaces, :player
 
   def initialize
     6.times do |x|
       5.times do |y|
         @spaces = {[x,y] => nil}
       end
-    end    
+    end  
+    @player = "white"
   end
+
+  def switch_player
+    if @player == "white"
+      @player = "black"
+    
+    elsif @player == "black"
+      @player = "white"
+    end
+  end
+
+  #adds to column, returns true if works, false if doesn't
+  def add_to(column)
+    
+    y = 0
+
+    until spaces[[column,y]].nil? do
+      y += 1
+    end
+
+    return false if y >= 6
+
+    @spaces[[column,y]] = @player
+
+    switch_player
+    
+    return true
+  
+  end
+
+  def won?
+    switch_player
+    player = @player
+    switch_player
+
+
+    win_directions = [
+      [-1, 1], [0, 1], [1, 1],
+      [-1, 0],         [1, 0],
+      [-1, -1], [0, -1], [1, -1]
+    ]
+
+    player_pieces = []
+
+    6.times do |x|
+      5.times do |y|
+        player_pieces << [x,y] if @spaces[[x,y]] == player
+      end
+    end
+
+    player_pieces.each do |loc|
+      connected = 1
+
+      win_directions.each do |win_direction|
+          #check if pieces around loc are also in player_pieces
+        connected = 1
+        3.times do
+          if player_pieces.include?([ loc[0]+ win_direction[0], loc[1]+ win_direction[1] ])
+            connected += 1
+            loc = [ loc[0]+ win_direction[0], loc[1]+ win_direction[1] ]
+          end
+          return true if connected == 4
+        end
+      end
+    end
+  return false 
+  end #won?
 
 
   def empty?
@@ -33,17 +111,59 @@ class Board
     return true
   end
 
+  def draw
+    puts ""
+    white = "●"
+    black = "◍"
+    5.downto(0) do |y|
+      6.times do |x|
+        print " | "
+        if @spaces[[x,y]] == "white"
+          print white
+        elsif @spaces[[x,y]] == "black"
+          print black
+        else
+          print "◌"
+        end
+      end
+      puts " |"
+    end
+    print "   1    2   3    4    5   6" 
+    puts "";puts ""
+    
+  end
+
 end
+
+def start_game
+  puts "    Welcome To Connect Four"
+  c4 = Connect4.new
+  until c4.check_win == true do
+    
+    c4.draw_board
+    puts "(#{c4.board.player}) Enter Move: "
+    c4.add_piece(q = gets.chomp.to_i-1)
+    return false if q < 0
+
+  end
+  c4.board.switch_player
+  c4.draw_board
+  puts "#{c4.board.player} Wins!"
+end
+
+start_game
 
 
 =begin
-| ◉ | ● | ○ |   |   |   |   | 
-| ◌ | ◌ | ◌ |   |   |   |   | 
-| ◦ |   |   |   |   |   |   | 
-| ◦ |   |   |   |   |   |   | 
-| ◦ |   |   |   |   |   |   | 
-| ◦ |   |   |   |   |   |   | 
 
+
+| ◉ | ● | ○ |   ◍ |   |   |   | 
+| ◌ | ◌ | ◌ |   |   |   |   | 
+| ◦ | - |   | . |   |   |   | 
+| ◦ | - | . |   |   |   |   | 
+| ◦ | - |   |   |   |   |   | 
+| ◦ | - |   |   |   |   |   | 
+  0   1   2   3   4
 7x6
 
 [0..6, 0..5]
--- spec/connect4_spec.rb
+++ spec/connect4_spec.rb
@@ -3,38 +3,117 @@ require 'Connect4'
 
 describe Connect4 do 
 
-  before do
+
+
+  describe "Setup" do
+
     c4 = Connect4.new
-  end
+    board = c4.board
 
-
-  describe '.create_board' do
-
-    #c4 = Connect4.new
 
     it "returns board" do
-      expect(c4.create_board).to be_instance_of(Board)
+      expect(c4.board).to be_instance_of(Board)
     end
 
-    b = c4.create_board
-
     it "is empty" do
-      expect(b.empty?).to eql(true)
+      expect(board.empty?).to eql(true)
       end 
 
   end #create_board
 
   describe ".add_piece" do
-    context "first piece in coloumn 1" do
-      it "makes the board at space [0,0] white" do
-        c4 = Connect4.new
-        board = c4.create_board
-        c4.add_piece(0)
+    
+    before(:each) do
+        @c4 = Connect4.new
+    end
 
-        expect(board.spaces[[0,0]]).to eql("white")
+    context "with first piece in column 1" do
+      it "makes the board at space [0,0] white" do
+        @c4.add_piece(0)  
+        expect(@c4.board.spaces[[0,0]]).to eql("white")
+      end
+    end
+
+    context "with a second(black) piece in column 1" do
+      it "makes the board at space [0,1] black" do
+        @c4.add_piece(0)
+        @c4.add_piece(0)
+        expect(@c4.board.spaces[[0,1]]).to eql("black")
+      end
+    end
+
+    context "with column one full" do
+      it "returns false" do
+        6.times do @c4.add_piece(0) end
+        expect(@c4.add_piece(0)).to eql(false)
       end
     end
   end #add_piece
+
+  describe ".check_win" do
+
+    before(:each) do
+      @c4 = Connect4.new
+    end
+
+    context "with no win on board" do 
+      it "returns false" do
+        expect(@c4.check_win).to eql(false)
+      end
+    end
+
+    context "with 4 whites in column 1" do
+      it "returns true" do
+        4.times do 
+          @c4.add_piece(0) 
+          @c4.board.switch_player
+        end
+        @c4.board.switch_player
+        expect(@c4.check_win).to eql(true)
+      end
+    end
+
+    context "with 4 blacks in col 1-4" do
+      it "returns true" do
+        @c4.add_piece(6)
+        4.times do |i|
+          @c4.add_piece(i)
+          @c4.board.switch_player
+        end
+        @c4.board.switch_player
+        expect(@c4.check_win).to eql(true)
+      end
+    end
+
+    context "with 4 whites along a diagonal from [0,0] to [3,3]" do
+      it "returns true" do
+        #add black
+        3.times do |i|
+          (i+1).times do |x|
+            @c4.board.switch_player
+            @c4.add_piece(i+1)
+          end
+        end
+        4.times do |x|
+          @c4.add_piece(x)
+          @c4.board.switch_player
+        end
+        @c4.board.switch_player
+        expect(@c4.check_win).to eql(true)
+      end
+    end
+  end #check_win
+
+  describe ".draw_board" do
+    it "looks cool" do
+      c4 = Connect4.new
+      3.times do |x| c4.add_piece(x) end
+      2.times do c4.add_piece(2) end
+      c4.add_piece(0)
+      c4.draw_board
+      except(true).to eql(false)
+    end
+  end
 
 
 end
